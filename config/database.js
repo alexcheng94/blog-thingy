bf3a5bf031bf91d9951953ec2cf5dabeceb1af34
Alex Cheng <alexcheng94@126.com>
2017-10-07 16:35:10 +0800

    new

--- config/database.js
+++ config/database.js
@@ -1,13 +1,16 @@
-//figure out which set of database credentials to return
-if (process.env.NODE_ENV === 'production') {
-	// in production, return prod set of database credentials
-	module.exports = {
+// //figure out which set of database credentials to return
+// if (process.env.NODE_ENV === 'production') {
+// 	// in production, return prod set of database credentials
+// 	module.exports = {
+// 		database: process.env.MONGO_URI
+// 	}
+// }else {
+// 	// in development, return development database credentials
+// 	module.exports = {
+// 		database: 'mongodb://localhost:27017/nodekb',
+// 		secret: 'yoursecret'
+// 	};
+// }
+module.exports = {
 		database: process.env.MONGO_URI
 	}
-}else {
-	// in development, return development database credentials
-	module.exports = {
-		database: 'mongodb://localhost:27017/nodekb',
-		secret: 'yoursecret'
-	};
-}
