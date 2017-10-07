36773a3fe89d6208bb828d75f80eae4980e6d332
Alex Cheng <alexcheng94@126.com>
2017-10-07 15:46:11 +0800

    yay

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	database: 'mongodb://alex:password@ds013405.mlab.com:13405/blog-thingy'
+}
---
+++ index.js
@@ -0,0 +1,132 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const config = require('./config/database');
+
+
+
+mongoose.connect(config.database);
+let db = mongoose.connection;
+
+// Check connection
+db.once('open',()=>{
+	console.log('You are now connected to MongoDB');
+});
+
+// Check for DB errors
+db.on('error',(err)=>{
+	console.log(err);
+});
+
+// Init App
+const app = express();
+
+// Bring in  Model
+let Article = require('./models/article');
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session Middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+	errorFormatter: (param, msg, value)=>{
+			var namespace = param.split('.')
+			, root = namespace.shift()
+			,formParam = root;
+		while(namespace.length){
+			formParam += '[' + namespace.shift() + ']';
+		}
+		return {
+			param: formParam,
+			msg: msg,
+			value: value
+		};
+	}
+}));
+
+
+//Passport Config
+require('./config/passport')(passport);
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('*',(req,res,next)=>{
+	res.locals.user = req.user || null;
+	next();
+});
+
+// Houme Route
+app.get('/',(req,res)=>{
+	Article.find({}, (err,articles)=>{
+		if (err) {
+			console.log(err);
+		}else{
+			res.render('index',{
+				title: 'Hello from Express',
+				articles: articles
+			});
+		}
+	});
+});
+
+// Route Files
+let articles = require('./routes/articles');
+app.use('/articles', articles);
+
+let users = require('./routes/users');
+app.use('/users', users);
+
+// Start Server 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
