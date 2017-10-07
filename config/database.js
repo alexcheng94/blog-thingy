f97073096a16d7204c94039f39b26c7fc6479781
Alex Cheng <alexcheng94@126.com>
2017-10-07 16:30:04 +0800

    new

--- config/database.js
+++ config/database.js
@@ -1,4 +1,13 @@
-module.exports = {
-	database: 'mongodb://localhost:27017/nodekb',
-	secret: 'yoursecret'
-};
+//figure out which set of database credentials to return
+if (process.env.NODE_ENV === 'production') {
+	// in production, return prod set of database credentials
+	module.exports = {
+		database: process.env.MONGO_URI
+	}
+}else {
+	// in development, return development database credentials
+	module.exports = {
+		database: 'mongodb://localhost:27017/nodekb',
+		secret: 'yoursecret'
+	};
+}
--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	database: 'mongodb://alex:password@ds013405.mlab.com:13405/blog-thingy'
-}
